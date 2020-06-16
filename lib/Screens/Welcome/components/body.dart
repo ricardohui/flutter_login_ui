75d86b63eea1b02b6266a9f67a7abe03a76875e2
Ricardo Hui <rcdhui@gmail.com>
2020-06-16 13:30:43 +0800

    finish welcome screen

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/Screens/Login/login_screen.dart';
+import 'package:flutter_login_ui/components/rounded_button.dart';
 import 'background.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_login_ui/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -17,7 +21,21 @@ class Body extends StatelessWidget {
           SvgPicture.asset(
             "assets/icons/chat.svg",
             height: size.height * 0.45,
-          )
+          ),
+          RoundedButton(
+            text: "LOGIN",
+            press: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return LoginScreen();
+              }));
+            },
+          ),
+          RoundedButton(
+            text: "LOGIN",
+            color: kPrimaryLightColor,
+            textColor: Colors.black,
+            press: () {},
+          ),
         ],
       ),
       size: size,
