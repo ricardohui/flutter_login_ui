240cf33e01c55746418e8c7bd85a5a4eba86b872
Ricardo Hui <rcdhui@gmail.com>
2020-06-16 12:44:54 +0800

    add background

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'background.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "WELCOME TO EDU",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SvgPicture.asset(
+            "assets/icons/chat.svg",
+            height: size.height * 0.45,
+          )
+        ],
+      ),
+      size: size,
+    );
+  }
+}
