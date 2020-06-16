71b83032714a6ef34f4917e8f1fac34eaed5b443
Ricardo Hui <rcdhui@gmail.com>
2020-06-16 16:10:48 +0800

    implement sign up

---
+++ lib/Screens/SignUp/signup_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/Screens/SignUp/components/body.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Body());
+  }
+}
